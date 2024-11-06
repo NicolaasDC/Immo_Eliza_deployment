ab4066d04cff7959d68279e6df51a3931ad026a3
NicolaasDC <167005737+NicolaasDC@users.noreply.github.com>
2024-11-06 16:53:24 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,24 @@
 import streamlit as st
 
-st.title('🤖House price prediction🤖')
+st.title('🧱House price prediction🧱')
 
 st.info('This is a machine learning app')
 
-st.write('You want to know the value of a house')
+st.write('You want to know the value of a house?')
 zip_code = st.text_input("What is the zip code?")
+
+zip_code = st.text_input("What is the construction year?")
+
+zip_code = st.text_input("What is the number of rooms?")
+
+zip_code = st.text_input("What is the living area?")
+
+zip_code = st.text_input("Is a kitchen equipped?")
+
+zip_code = st.text_input("What is the primary energy consumption?")
+
+zip_code = st.text_input("Does the house have double glazing?")
+
+zip_code = st.text_input("What is the state of the building?")
+
+zip_code = st.text_input("Is it a house or an apartement?")
