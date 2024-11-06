913e8819617cf036ce2386d8d6eb314dc5d0582e
NicolaasDC <167005737+NicolaasDC@users.noreply.github.com>
2024-11-06 16:21:27 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.write('Hello World')
+st.title('🤖House price prediction🤖')
+
+st.info('This is a machine learning app')
+
+st.write('You want to know the value of a house')
 zip_code = st.text_input("What is the zip code?")
