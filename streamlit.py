7fedc4d6d8f12a7b0e4a2daa2d2f0ac3fcfd0381
NicolaasDC <167005737+NicolaasDC@users.noreply.github.com>
2024-11-06 16:59:54 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,18 +7,18 @@ st.info('This is a machine learning app')
 st.write('You want to know the value of a house?')
 zip_code = st.text_input("What is the zip code?")
 
-zip_code = st.text_input("What is the construction year?")
+construction_year = st.text_input("What is the construction year?")
 
-zip_code = st.text_input("What is the number of rooms?")
+number_rooms = st.text_input("What is the number of rooms?")
 
-zip_code = st.text_input("What is the living area?")
+living_area = st.text_input("What is the living area?")
 
-zip_code = st.text_input("Is a kitchen equipped?")
+kitchen = st.text_input("Is a kitchen equipped?")
 
-zip_code = st.text_input("What is the primary energy consumption?")
+primary_energy_consumption = st.text_input("What is the primary energy consumption?")
 
-zip_code = st.text_input("Does the house have double glazing?")
+double_glazing = st.text_input("Does the house have double glazing?")
 
-zip_code = st.text_input("What is the state of the building?")
+state_building = st.text_input("What is the state of the building?")
 
-zip_code = st.text_input("Is it a house or an apartement?")
+type_house = st.text_input("Is it a house or an apartement?")
