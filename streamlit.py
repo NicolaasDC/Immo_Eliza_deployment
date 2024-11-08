742e62dcbd0dafb8a7c50acdc42edf3b47a67488
NicolaasDC <nicolaasdeclercq@hotmail.com>
2024-11-08 14:33:29 +0100

    Updated try except blocks

--- streamlit.py
+++ streamlit.py
@@ -35,46 +35,52 @@ st.info('This is an app to calculate the price of a house using a machine learni
 st.write('You want to know the value of a house?')
 try:
     zip_code = int(st.text_input("What is the zip code?"))
-
-    construction_year = int(st.text_input("What is the construction year?"))
-
-    number_rooms = int(st.text_input("What is the number of rooms?"))
-
-    living_area = float(st.text_input("What is the living area? (m²)"))
-
-    kitchen = st.selectbox("Is a kitchen equipped?", ("Yes", "No"), index=None,
-        placeholder="Select kitchen")
-    if kitchen ==  'Yes':
-        kitchen_enc = 1
-    else:
-        kitchen_enc = 0
-
-    primary_energy_consumption = float(st.text_input("What is the primary energy consumption? (kWh/m²)", value=0))
-
-    double_glazing = st.selectbox("Does the house have double glazing?",('Yes', 'No'), index=None, placeholder="Select double glazing")
-    if double_glazing ==  'Yes':
-        double_glazing_enc = 1
-    else:
-        double_glazing_enc = 0
-
-    state_building = st.selectbox("What is the state of the building?", ('To restore', 'To renovate', 'To be done up', 'Good', 'Just renovated', 'As new'), index=None, placeholder="Select state of the building")
-    state_encoded = {'To restore': 0, 'To renovate': 1, 'To be done up': 2, 'Good': 3, 'Just renovated': 4, 'As new': 5}
-
-    state_building_enc = state_encoded.get(state_building, 0)
-
-
-    type_house = st.selectbox("Is it a house or an apartement?", ('House', 'Apartment'), index=None, placeholder="Select house or apartment")
-    if type_house == 'House':
-        type_house_enc = 1
-    else:
-        type_house_enc = 0
-    
-        # Place the button in the Streamlit app
-    if st.button("Click to calculate price"):
-        # Call the function when the button is clicked
-        price = calculate_price(zip_code, construction_year, number_rooms, living_area, kitchen_enc, primary_energy_consumption, double_glazing_enc, state_building_enc, type_house_enc)   
-        st.write("Your predicted price: €", round(price, 2))
-        
 except ValueError as ve:
-    st.error(f"Please enter valid numeric values for all inputs: {ve}")
+    st.error(f"Please enter a valid numeric value: {ve}")
+try:
+    construction_year = int(st.text_input("What is the construction year?"))
+except ValueError as ve:
+    st.error(f"Please enter a valid numeric value: {ve}")
+try:
+    number_rooms = int(st.text_input("What is the number of rooms?"))
+except ValueError as ve:
+    st.error(f"Please enter a valid numeric value: {ve}")
+try:
+    living_area = float(st.text_input("What is the living area? (m²)"))
+except ValueError as ve:
+    st.error(f"Please enter a valid numeric value: {ve}")
+    
+kitchen = st.selectbox("Is a kitchen equipped?", ("Yes", "No"), index=None,
+    placeholder="Select kitchen")
+if kitchen ==  'Yes':
+    kitchen_enc = 1
+else:
+    kitchen_enc = 0
+
+primary_energy_consumption = float(st.text_input("What is the primary energy consumption? (kWh/m²)", value=0))
+
+double_glazing = st.selectbox("Does the house have double glazing?",('Yes', 'No'), index=None, placeholder="Select double glazing")
+if double_glazing ==  'Yes':
+    double_glazing_enc = 1
+else:
+    double_glazing_enc = 0
+
+state_building = st.selectbox("What is the state of the building?", ('To restore', 'To renovate', 'To be done up', 'Good', 'Just renovated', 'As new'), index=None, placeholder="Select state of the building")
+state_encoded = {'To restore': 0, 'To renovate': 1, 'To be done up': 2, 'Good': 3, 'Just renovated': 4, 'As new': 5}
+
+state_building_enc = state_encoded.get(state_building, 0)
+
+type_house = st.selectbox("Is it a house or an apartement?", ('House', 'Apartment'), index=None, placeholder="Select house or apartment")
+if type_house == 'House':
+    type_house_enc = 1
+else:
+    type_house_enc = 0
+    
+# Place the button in the Streamlit app
+if st.button("Click to calculate price"):
+# Call the function when the button is clicked
+    price = calculate_price(zip_code, construction_year, number_rooms, living_area, kitchen_enc, primary_energy_consumption, double_glazing_enc, state_building_enc, type_house_enc)   
+    st.write("Your predicted price: €", round(price, 2))
+        
+
     
